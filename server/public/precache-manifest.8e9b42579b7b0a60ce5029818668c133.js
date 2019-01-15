c036ed4c18055da97bc90ea5b68b5e477ba06f93
Ian Hundere <ianfundere@gmail.com>
2019-01-15 18:21:37 -0500

    adds build

---
+++ server/public/precache-manifest.8e9b42579b7b0a60ce5029818668c133.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08c9ac1ccfef898b31544b90d1b3ec6",
+    "url": "/static/media/housedrop.d08c9ac1.wav"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0985d44d12257813d653",
+    "url": "/static/js/main.0985d44d.chunk.js"
+  },
+  {
+    "revision": "c6f7e263c2f390a855e8",
+    "url": "/static/js/1.c6f7e263.chunk.js"
+  },
+  {
+    "revision": "0985d44d12257813d653",
+    "url": "/static/css/main.9e86e141.chunk.css"
+  },
+  {
+    "revision": "c6f7e263c2f390a855e8",
+    "url": "/static/css/1.3a2d460c.chunk.css"
+  },
+  {
+    "revision": "53e41d9c05a93f650dea31c736c490e6",
+    "url": "/index.html"
+  }
+];
